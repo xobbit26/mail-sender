acc5b04a93fdf9c031c8fb93b2e2300482653abc
Станислав Наравецкас <xobbit26@gmail.com>
2021-12-06 13:24:43 +0300

    removed unnecessry parts of code

--- src/main/java/com/example/mail_sender/MailSenderApplication.java
+++ src/main/java/com/example/mail_sender/MailSenderApplication.java
@@ -1,17 +1,12 @@
 package com.example.mail_sender;
 
-import com.example.mail_sender.enums.TestEnum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class MailSenderApplication {
 
     public static void main(String[] args) {
-        System.out.println(TestEnum.VALUE_ONE.getValue());
-        System.out.println(TestEnum.VALUE_TWO.getValue());
-
         SpringApplication.run(MailSenderApplication.class, args);
     }
 
